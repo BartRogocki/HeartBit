e8cdf2180940a29a84ec145af00b0692d344f057
bartrogocki <bartrogocki@gmail.com>
2018-11-09 22:31:44 +0100

    create first view

---
+++ HeartBit.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(400, 100);
+    background(0);
+}
+
+var x = 0;
+var y = 50;
+var speed = 2;
+var dir = true;
+function draw() {
+    let c = y;
+    stroke(255);
+    point(x++, y);
+    if (x > width) {
+        x = 0;
+        background(0);
+    }
+    if (y == 75) {
+        dir = false;
+    }
+    if (y == 25) {
+        dir = true;
+    }
+    if (dir == true) {
+        y++;
+    } else {
+        y--;
+    }
+
+
+
+
+}
